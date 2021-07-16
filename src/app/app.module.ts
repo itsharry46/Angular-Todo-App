1a643ceb0724e7817aac68ab0f765922d998014f
Hariharan Achary <55319339+itsharry46@users.noreply.github.com>
2021-07-16 06:58:40 +0530

    Netlify fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
 
 import { MatDialogModule } from '@angular/material/dialog';
-import { TooltipDirective } from './shared/tooltip.directive';
  
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoItemComponent,
-    EditTodoDialogComponent,
-    TooltipDirective
+    EditTodoDialogComponent
   ],
   imports: [
     BrowserModule,
